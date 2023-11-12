1298e37d619b9c371ba7072f059e26017f0d2b1d
ARGamer36 <ajrodz2004@gmail.com>
2023-11-11 22:35:28 -0500

    Initial Commit

---
+++ Driver/src/main/java/Commands/MercyMain.java
@@ -0,0 +1,11 @@
+package Commands;
+
+import Main.MainCommands;
+
+public class MercyMain extends MainCommands {
+    public MercyMain(String version) {
+        super(version);
+        //prefixCommands.add();
+        //slashCommands.add();
+    }
+}
--- Driver/src/main/java/MercyBot.java
+++ Driver/src/main/java/MercyBot.java
@@ -1,5 +1,26 @@
-public class MercyBot {
+import Commands.MercyMain;
+import Main.BotDriver;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class MercyBot extends BotDriver {
+    @Override
+    protected void setup() {
+        VERSION = "1.0";
+        activity = Activity.playing("Overwatch 3");
+        TOKEN = "TEST_BOT";
+    }
+
+    public MercyBot() throws LoginException {
+        super();
+        shardManager.addEventListener(new MercyMain(VERSION));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        try {
+            MercyBot bot = new MercyBot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: provided bot token invalid");
+        }
     }
 }
